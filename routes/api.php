852ae882bac2fb2b89def2f97925dc4ee0eb920a
YATACHI <60470788+p2510@users.noreply.github.com>
2022-12-15 14:21:00 +0100

    Route
    change route method to resource from apiResource

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('story', StoryController::class);
+Route::apiResource('story', StoryController::class);
